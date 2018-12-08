eb61bc4f9a6501a357aa7bad2085d46486e4be67
ouno <lxdzealot@gmail.com>
2018-12-08 20:09:56 +0800

    add filtered_values

--- model02.py
+++ model02.py
@@ -1,3 +1,9 @@
 d = {'key': 'value'}
 print(d.get('key', 'default_value'))
 print(d.get('key2', 'default_value'))
+
+
+sequence = [1, 2, 3, 4, 5, 6]
+filtered_values = [value for value in sequence if value != 6]
+
+print(filtered_values)
